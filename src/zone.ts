862da22caa1596519d427a1dc8976be71b0b3c7a
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2022-01-16 03:45:30 +0800

    Export zone code so that can be use elsewhere

--- src/zone.ts
+++ src/zone.ts
@@ -1,4 +1,4 @@
-enum ZoneCode {
+const enum ZoneCode {
   jhr01 = 'jhr01',
   jhr02 = 'jhr02',
   jhr03 = 'jhr03',
@@ -59,3 +59,5 @@ enum ZoneCode {
   wly01 = 'wly01',
   wly02 = 'wly02',
 }
+
+export default ZoneCode;
