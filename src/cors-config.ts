794c61368cbe177de701e12a5d948dbc0c0ebae5
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2023-01-28 09:34:51 +0800

    Allow anyone to use the API

--- src/cors-config.ts
+++ src/cors-config.ts
@@ -2,20 +2,26 @@ import { FastifyCorsOptions } from "fastify-cors";
 
 let corsConfig: FastifyCorsOptions;
 
-const allowedOrigins: (string | RegExp)[] = [
-  /\.fajarhac\.com$/i,
-  'fajarhac.com',
-  // /localhost/i,
-]
+// const allowedOrigins: (string | RegExp)[] = [
+//   /\.fajarhac\.com$/i,
+//   'fajarhac.com',
+//   // /localhost/i,
+// ]
 
-if (/development/i.test(process.env.NODE_ENV ?? '')) {
-  corsConfig = {
-    origin: '*',
-  }
-} else {
-  corsConfig = {
-    origin: allowedOrigins
-  }
+// if (/development/i.test(process.env.NODE_ENV ?? '')) {
+//   corsConfig = {
+//     origin: '*',
+//   }
+// } else {
+//   corsConfig = {
+//     origin: allowedOrigins
+//   }
+// }
+
+// This is just read API so allow all by default
+
+corsConfig = {
+  origin: '*'
 }
 
 export default corsConfig;
